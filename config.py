c540401d5d0fda896f3c0960d2d829fd1ce5b614
amyxie361 <amyxie361@outlook.com>
2020-05-09 13:04:15 -0400

    tree loss runable

--- config.py
+++ config.py
@@ -4,7 +4,8 @@ glove_path = "/data/yqxie/00_data/GloVe"
 train_src_file = squad_path + "/train_srcs.pkl"
 # train_trg_file = squad_path + "/tgt-train.txt"
 # dev file
-dev_src_file = squad_path + "/dev_srcs.pkl"
+#dev_src_file = squad_path + "/dev_srcs.pkl"
+dev_src_file = squad_path + "/train_srcs.pkl"
 # dev_trg_file = squad_path + "/tgt-dev.txt"
 # test file
 # test_src_file = squad_path + "/para-test.txt"
@@ -19,19 +20,19 @@ vocab_file = "../data/vocab.txt"
 dev_tree_file = "../data/squad_dev_dependency_parse.txt"
 
 model_path = "./save/"
-exp_name = "loss_ver1_alpha0.5"
-model_path = "./save/" + exp_name + "/train_403160221/1_6.08"
-train = True
-test = False
-device = "cuda:5"
+exp_name = "loss_ver1_alpha0"
+model_path = "./save/" + exp_name + "/train_509110126/354_15.01"
+train = False
+test = True
+device = "cuda:7"
 use_gpu = True
-debug = False
+debug = True
 debug_num = 100
 vocab_size = 45000
 freeze_embedding = False
 sparsity = False
 
-num_epochs = 20
+num_epochs = 1000
 max_seq_len = 400
 num_layers = 2
 hidden_size = 300
@@ -44,7 +45,7 @@ batch_size = 1
 dropout = 0.3
 max_grad_norm = 20.0
 
-tree_loss_alpha = 0.5
+tree_loss_alpha = 0.0
 
 use_tag = True
 use_pointer = True
